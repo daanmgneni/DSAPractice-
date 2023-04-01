b52819ac01472040991f14000dc0186dc51b53cf
DAAN <daanmgneni@gmail.com>
2023-04-01 09:18:44 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BinarySearchTree
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structures and Algorithms Practice Problem");
+
+        }
+    }
+}
+namespace BinarySearchTree
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structures and Algorithms Practice Problem");
+
+        }
+    }
+}
